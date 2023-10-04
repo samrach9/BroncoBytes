8816665c5c25fde3a72c675a86a0ea1befc6c8c2
madeleinedoi <maddoi@hotmail.com>
2023-10-03 21:13:20 -0700

    Initial Backend Implementation

---
+++ backend/getFood/app.py
@@ -0,0 +1,39 @@
+import json
+import os
+import boto3
+from dynamodb_json import json_util as json_db
+
+#getAllFood
+def lambda_handler(event, context):
+    print('received event: ')
+    print(event)
+
+    # Static Variables
+    db_client = boto3.client('dynamodb')
+    DYNAMODB_TABLE = os.environ['DYNAMODB_TABLE']
+
+    body = {}
+    if event['body']:
+        body = json.loads(event['body'])
+        response = db_client.get_item(
+            TableName=DYNAMODB_TABLE,
+            Key={
+                'foodId':{'S': body['foodId']},
+            },
+        ) 
+        body = {"food": json_db.loads(response['Item'])}
+    else:        
+        response = db_client.scan(
+            TableName=DYNAMODB_TABLE,
+        )
+        body = {"foods": json_db.loads(response['Items'])}
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        "body": json.dumps(body),
+    }
---
+++ backend/getReview/app.py
@@ -0,0 +1,58 @@
+import os
+import json
+import boto3
+from dynamodb_json import json_util as json_db
+
+# getReview
+def lambda_handler(event, context):
+
+    print('received event:')
+    print(event)
+
+    # Static Variables
+    db_client = boto3.client('dynamodb')
+    DYNAMODB_TABLE = os.environ['DYNAMODB_TABLE']
+ 
+    body = json.loads(event['body'])
+     
+    # Get review(s)
+    if 'reviewId' in body:
+        response = db_client.get_item(
+            TableName=DYNAMODB_TABLE,
+            Key={
+                'reviewId':{'S': body['reviewId']},
+            },
+        ) 
+    elif 'userId' in body:
+        response = db_client.query(
+            TableName=DYNAMODB_TABLE,
+            IndexName='userIdIndex',
+            KeyConditionExpression='userId = :userId',
+            ExpressionAttributeValues={
+                ':userId': {'S': body['userId']},
+            },
+        ) 
+    elif 'foodId' in body:
+        response = db_client.query(
+            TableName=DYNAMODB_TABLE,
+            IndexName='foodIdIndex',
+            KeyConditionExpression='foodId = :foodId',
+            ExpressionAttributeValues={
+                ':foodId': {'S': body['foodId']},
+            },
+        ) 
+
+    if 'Item' in response:
+        body = {"reviews": [json_db.loads(response['Item'])]}
+    elif 'Items' in response:
+        body = {"reviews": json_db.loads(response['Items'])}
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        "body": json.dumps(body),
+    }
