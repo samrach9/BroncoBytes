7bcac0e72e51e74bdad8fbde5ea15174958c8b40
Jessie Jiang <jessie.jiang22@gmail.com>
2023-11-27 21:51:04 -0800

    modified styling for friends page

--- frontend/components/friendsList.js
+++ frontend/components/friendsList.js
@@ -33,13 +33,13 @@ export function FriendsList({ friends }) {
               <Image
                 key={friend.userId}
                 source={{ uri: friend.photoUrl }}
-                style={styles.circle}
+                style={[styles.circle, { padding: 20 }]}
               />
             ) : (
               <Image
                 key={friend.userId}
                 source={require("../assets/defaultProfilePicture.png")}
-                style={styles.circle}
+                style={[styles.circle, { padding: 20 }]}
               />
             )}
             <Text key={friend.userId} style={styles.friendText}>
@@ -56,30 +56,32 @@ const styles = StyleSheet.create({
   friendsContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    margin: 15,
-    marginLeft: 25,
-    marginRight: 25,
+    paddingLeft: 25,
+    paddingRight: 25,
     alignSelf: "flex-start",
   },
   friendContainer: {
     flexDirection: "column",
     alignItems: "center",
+    padding: 25,
   },
   circle: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    width: 125,
+    height: 125,
+    borderRadius: 75,
     backgroundColor: "#B30738",
     borderColor: "black",
     borderWidth: 2,
     justifyContent: "center",
     alignItems: "center",
     padding: 10,
+
   },
   friendText: {
     fontFamily: "Bungee",
     fontSize: 15,
     color: "#FFFFFF",
-    padding: 5,
+    textAlign: 'center',
+    paddingTop: 10,
   },
 });
